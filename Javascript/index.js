dfa8913443e96b10fc42d4bcfb4a8ee54d39a107
Nurullazizahhh <146433189+Nurullazizahhh@users.noreply.github.com>
2023-10-07 22:15:48 +0800

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,68 @@
+//if, else, dan nested if
+let usia = 18;
+
+if (usia >= 18) {
+  console.log("Anda adalah dewasa.");
+  alert ("Anda adalah dewasa.")
+} else {
+  console.log("Anda adalah anak-anak.");
+}
+
+//Contoh nested if
+// let jenisKelamin = "wanita";
+
+// if (usia = 18) {
+//   if (jenisKelamin === "wanita") {
+//     console.log("Anda adalah wanita dewasa.");
+//     alert ("Anda Wanita Dewasa")
+//   } else {
+//     console.log("Anda adalah pria dewasa.");
+//   }
+// }
+
+//switch case
+// let hari = "Senin";
+
+// switch (hari) {
+//   case "Senin":
+//     console.log("Hari ini adalah hari kerja.");
+//     alert ("Hari ini adalah hari kerja.")
+//     break;
+//   case "Sabtu":
+//   case "Minggu":
+//     console.log("Hari ini adalah hari libur.");
+//     break;
+//   default:
+//     console.log("Hari tidak valid.");
+// }
+
+//for statement
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Iterasi ke-" + i);
+//     alert ("Iterasi ke-" + i)
+//   }
+
+// //while
+// let angka = 1;
+// while (angka <= 5) {
+//   console.log("Angka: " + angka);
+//    alert ("Angka: " + angka)
+//   angka++;
+// }
+
+//do while
+// let x = 1;
+// do {
+//   console.log("Nilai x: " + x);
+//  alert ("Nilai x: " + x)
+//   x++;
+// } while (x <= 5);
+
+//function
+// function tambah(a, b) {
+//     return a + b;
+//   }
+
+//   let hasilPenambahan = tambah(3, 5);
+//   console.log("Hasil penambahan: " + hasilPenambahan);
+//   alert ("Hasil penambahan: " + hasilPenambahan)
